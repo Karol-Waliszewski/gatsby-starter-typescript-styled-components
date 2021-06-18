a23f9f50eb39722c7e074b14a03c595e95fd60f0
Karol-Waliszewski <karolwaliszewski@gmail.com>
2021-06-19 00:56:46 +0200

    Added global styles

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react'
+import styled from 'styled-components'
+
+import GlobalStyles from 'styles/global'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 2rem 1rem;
+`
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <GlobalStyles />
+      <Container>
         <main>{children}</main>
         <footer
           style={{
@@ -24,7 +28,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
-      </div>
+      </Container>
     </>
   )
 }
